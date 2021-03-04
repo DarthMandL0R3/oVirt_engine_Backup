ea6daeb0cfdbd640f829c73f09e9c0505bd7f43f
abrar <abrar@localhost>
2021-03-04 13:57:27 +0800

    update the seperate logs action

--- rhev_bkup.sh
+++ rhev_bkup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-engine-backup --mode=backup --scope=all --file="/var/lib/ovirt-engine/backups/fullbackup-$(date +%Y%m%d%H%M%S).tar.bz2" --log=/var/log/ovirt-engine/fullbackup-$(date +%Y%m%d%H%M%S).log 1> /tmp/ovirt-engine_backup.log 2> /tmp/ovirt-engine_error.log
+engine-backup --mode=backup --scope=all --file="/var/lib/ovirt-engine/backups/fullbackup-$(date +%Y%m%d%H%M%S).tar.bz2" --log=/var/log/ovirt-engine/fullbackup-$(date +%Y%m%d%H%M%S).log 1>/tmp/ovirt-engine_backup.log 2>/tmp/ovirt-engine_error.log
 wait
 exit 0
